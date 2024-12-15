7acbdc545024b686706e95bf2a48cdcfc4960ca6
furkan-firat <furkanfirat@live.com>
2024-12-15 20:54:15 +0300

    add redux-logger

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
+import pkg from 'redux-logger';
 import { cakeReducer } from './features/cake/cakeSlice.js';
 import { iceCreamReducer } from './features/icecream/iceCreamSlice.js';
+
+const { createLogger } = pkg;
+
+const logger = createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCream: iceCreamReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -2,11 +2,7 @@ import store from './app/store.js';
 import { cakeActions } from './app/features/cake/cakeSlice.js';
 import { iceCreamActions } from './app/features/icecream/iceCreamSlice.js';
 
-console.log('Initial State: ', store.getState());
-
-const unsubscribe = store.subscribe(() => {
-  console.log('State updated!', store.getState());
-});
+const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
