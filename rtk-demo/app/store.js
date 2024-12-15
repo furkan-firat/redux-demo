aa7ff2aab0e0a82ac27e3ea5a81fc29d5f82fa03
furkan-firat <furkanfirat@live.com>
2024-12-16 00:25:20 +0300

    add user feature & async operations

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import pkg from 'redux-logger';
 import { cakeReducer } from './features/cake/cakeSlice.js';
 import { iceCreamReducer } from './features/icecream/iceCreamSlice.js';
+import { userReducer } from './features/user/userSlice.js';
 
 const { createLogger } = pkg;
 
@@ -11,6 +12,7 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCream: iceCreamReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,12 +1,15 @@
 import store from './app/store.js';
 import { cakeActions } from './app/features/cake/cakeSlice.js';
 import { iceCreamActions } from './app/features/icecream/iceCreamSlice.js';
+import { fetchUsers } from './app/features/user/userSlice.js';
 
 const unsubscribe = store.subscribe(() => {});
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(1));
+store.dispatch(fetchUsers());
 
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(1));
+
+// unsubscribe();
